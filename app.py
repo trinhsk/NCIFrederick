1c52b5fdae9482c9d3feaa9d2947693f0f2c4acc
Spencer <sktrinh12@gmail.com>
2019-04-25 00:18:23 -0400

    384 abs lineplot

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
-from flask import Flask, render_template
-from graph import build_graph, pd
- 
+from flask import Flask, render_template, Response, Markup, send_file
+from graph import build_graph, pd, stripDF
+
 app = Flask(__name__)
 
-dfm100 = pd.read_csv('/users/trinhsk/Documents/absVizFlask/textfiles/14190301100.txt',encoding='utf-16',sep='\t',skiprows=2,skipfooter=2,engine='python')
+dfm100 = pd.read_csv(app.root_path + "/textfiles/14190301100.txt",encoding='utf-16',sep='\t',skiprows=2,skipfooter=2,engine='python')
+dfm100 = stripDF(dfm100)
 
-dfm200 = pd.read_csv('/users/trinhsk/Documents/absVizFlask/textfiles/14190301200.txt',encoding='utf-16',sep='\t',skiprows=2,skipfooter=2,engine='python')
+dfm200 = pd.read_csv(app.root_path+ '/textfiles/14190301200.txt',encoding='utf-16',sep='\t',skiprows=2,skipfooter=2,engine='python')
+dfm200 = stripDF(dfm200)
 
-@app.route('/graphs')
-def graphs():
-     
-    graph_url = build_graph(dfm);
- 
-    return render_template('graphs.html', absLnPlt=graph_url)
- 
+wavelength = list(range(220,810,10))
+
+lstofplots100 = []
+for i in range(384):
+    lstofplots100.append(Markup(build_graph(dfm100,wavelength,i).decode('utf-8')))
+
+@app.route('/')
+def plotgraphs():
+    return render_template('graphs.html', absLnPlt=lstofplots100)
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
